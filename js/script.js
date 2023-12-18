39a278d692e72db8f6880cf7d301d0fb05699c29
shinji-iseno <shinji.iseno.work@gmail.com>
2023-12-18 13:27:39 +0900

    スクロールインのjs修正

--- js/script.js
+++ js/script.js
@@ -48,16 +48,34 @@ $(function(){
 		});
 });
 
-// ふわっと表示
-$(function(){
-  $(window).scroll(function (){
-    $('.js-fade').each(function(){
-      var pos = $(this).offset().top;
-      var scroll = $(window).scrollTop();
-      var windowHeight = $(window).height();
-      if (scroll > pos - windowHeight + 100){
-        $(this).addClass('scrollIn');
-      }
-    });
-  });
-});
+
+// スクロールイン
+function fadeinEvent() {
+	const fadeinClass= Array.prototype.slice.call(document.getElementsByClassName("js-fade"));
+
+	fadeinClass.forEach(function( element ) {
+		// getBoundingClientRect で要素の位置や幅や高さなどを取得
+		const boundingClientRect = element.getBoundingClientRect();
+
+		// スクロールの位置情報（html のスクロールがなければ、body のスクロール）を取得
+		const scroll = document.documentElement.scrollTop || document.body.scrollTop;
+
+		// ブラウザウィンドウの ビューポートの高さ
+		const windowHeight = window.innerHeight;
+			// スクロールの位置が、フェードインしたい要素の位置にいるかどうか判定する
+			if (scroll > scroll + boundingClientRect.top - windowHeight + 100){
+
+				// 要素を表示する場合は、要素の透明度を無くし、Y方向の移動距離を無くす。これで表示される
+				element.style.opacity = "1";
+				element.style.transform = "translateY(0)";
+		}
+	});
+}
+
+// 画面がロードされたときに行う処理を記載
+window.onload = function(){
+// 画面が中途半端なスクロール位置でリロードされても表示するべきものが表示されるようにするため、ロードですぐに呼び出す
+	fadeinEvent();
+	// スクロールしたら動くエベントとして用意しておく。スクロールするたびに動くようにする
+	window.addEventListener('scroll', fadeinEvent, false);
+}
